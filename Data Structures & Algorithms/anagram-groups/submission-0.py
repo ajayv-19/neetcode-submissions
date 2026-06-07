56fa8552cb0dcd9391171b7dc1f2c736a818806c
ajayv-19 <68264798+ajayv-19@users.noreply.github.com>
2026-06-06 22:56:34 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashing = {}
+        for s in strs:
+            key = self.generate_key(s)
+            if key in hashing:
+                hashing[key].append(s)
+            else:
+                hashing[key] = [s]
+        output = []
+        for i, v in hashing.items():
+            output.append(v)
+        return output
+        
+    
+    def generate_key(self,string):
+            return tuple(sorted(string))
+
+        
